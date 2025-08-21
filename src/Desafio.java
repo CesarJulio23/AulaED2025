0d19c12a37d9d7e1bd5dbf50becced838d6a5f2d
Ivan Ferreira <ivan.marcelo.ferreira@gmail.com>
2025-08-20 22:01:18 -0300

    criando método lançar notas

--- src/Desafio.java
+++ src/Desafio.java
@@ -4,20 +4,38 @@ import java.util.Scanner;
 
 public class Desafio {
 
+    // declaração do scanner para entrada de dados
+    public static Scanner scanner = new Scanner(System.in);
+
+    public static void lancarNotas(List<String> nomes, int index, String avaliacao) {
+        System.out.println("Digite a nota da [" + avaliacao + "] do aluno " + nomes.get(index));
+        double nota = scanner.nextDouble();
+//        notas.add(nota);
+    }
+
     public static void main(String[] args) {
-        // declaração do scanner para entrada de dados
-        Scanner scanner = new Scanner(System.in);
+        System.out.println("Quantos alunos você deseja cadastrar?");
+        int tamanhoDaLista = scanner.nextInt();
+        scanner.nextLine(); // bug do java
 
         List<String> nomes = new ArrayList<>();
-        int tamanhoDaLista = 5;
+        List<Double> notas = new ArrayList<>();
+        List<Double> medias = new ArrayList<>();
 
         for (int i = 0; i < tamanhoDaLista; i++) {
             System.out.println("Digite um nome:");
             String nome = scanner.nextLine();
             nomes.add(nome);
         }
-
         System.out.println("Os nomes são: " + nomes);
+
+        // lançar as notas da prova 1
+        for (int i = 0; i < tamanhoDaLista; i++) {
+            lancarNotas(nomes, i, "Prova 1");
+//            lancarNotas(nomes, "Prova 2");
+//            lancarNotas(nomes, "Trabalho");
+        }
+
     }
 
 }
